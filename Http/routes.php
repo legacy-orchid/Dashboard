71d08f08df4361576bcbadef98b02a02ba3b38af
tabuna <bliz48rus@gmail.com>
2016-02-23 22:45:17 +0300

    Авторизация

--- Http/routes.php
+++ Http/routes.php
@@ -1,9 +1,14 @@
 <?php
 
-    Route::get('/', 'DashboardController@index');
+Route::get('/', 'DashboardController@index');
 
 
 Route::resource('settings', 'SettingsController', [
     'only' => [
         'index', 'show', 'store', 'update', 'destroy'
     ]]);
+
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
