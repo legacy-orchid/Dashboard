77ba1ac7e5d87a1908c5100343d6203cde6ed6be
tabuna <bliz48rus@gmail.com>
2016-02-26 20:26:30 +0300

    Авторизация\Регистрация

--- Resources/views/auth/emails/password.blade.php
+++ Resources/views/auth/emails/password.blade.php
@@ -1,2 +1,2 @@
 Click here to reset your password: <a
-        href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+        href="{{ $link = route('dashboard.password.reset',$token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
--- Resources/views/auth/login.blade.php
+++ Resources/views/auth/login.blade.php
@@ -7,7 +7,8 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Login</div>
                     <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                        <form class="form-horizontal" role="form" method="POST"
+                              action="{{ route('dashboard.auth.login') }}">
                             {!! csrf_field() !!}
 
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -54,7 +55,7 @@
                                         <i class="fa fa-btn fa-sign-in"></i>Login
                                     </button>
 
-                                    <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your
+                                    <a class="btn btn-link" href="{{ route('dashboard.password.reset') }}">Forgot Your
                                         Password?</a>
                                 </div>
                             </div>
