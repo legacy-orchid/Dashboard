714a6f39530f872e2847fbcd1b848c31187c8af4
tabuna <bliz48rus@gmail.com>
2016-02-29 22:44:16 +0300

    Разметка структуры

--- Resources/assets/js/app.js
+++ Resources/assets/js/app.js
@@ -5,9 +5,21 @@ app.config(['$routeProvider',function($routeProvider){
     $routeProvider.when('/admin', {
         templateUrl: '/build/partials/main/index.html',
         controller: 'mainPage'
-    }).when('/link2', {
-        templateUrl: '/build/partials/main/index2.html',
-        controller: 'secondPage'
+    }).when('/roles', {
+        templateUrl: '/orchid/tpl/systems/roles.html',
+        controller: 'systems.roles'
+    }).when('/users', {
+        templateUrl: '/orchid/tpl/systems/users.html',
+        controller: 'systems.users'
+    }).when('/language', {
+        templateUrl: '/orchid/tpl/systems/language.html',
+        controller: 'systems.language'
+    }).when('/domains', {
+        templateUrl: '/orchid/tpl/systems/domains.html',
+        controller: 'systems.domains'
+    }).when('/settings', {
+        templateUrl: '/orchid/tpl/systems/settings.html',
+        controller: 'systems.settings'
     }).otherwise({
         templateUrl:'/build/partials/main/index.html',
         controller: 'mainPage'
