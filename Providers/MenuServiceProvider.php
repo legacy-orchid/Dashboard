5b060128438ce935f8c509d1fb9f9a263de2dd39
tabuna <bliz48rus@gmail.com>
2016-03-09 01:06:01 +0300

    new elements

--- Providers/MenuServiceProvider.php
+++ Providers/MenuServiceProvider.php
@@ -24,6 +24,7 @@ class MenuServiceProvider extends ServiceProvider
 
     protected function registerMenu(DashboardMenu $dashboardMenu = null)
     {
+
         $domainsMenu = [
             'slug' => 'Web domains',
             'icon' => 'fa fa-globe',
@@ -44,16 +45,119 @@ class MenuServiceProvider extends ServiceProvider
             'divider' => true,
         ];
 
+
         $errorMenu = [
-            'slug' => 'Localization',
-            'icon' => 'fa fa-language',
-            'url' => route('dashboard.domains.index'), //'/dashboard/#!/settings', //route('settings'),
-            'label' => trans('dashboard::menu.Localization'),
+            'slug' => 'Logs',
+            'icon' => 'fa fa-bug',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Logs'),
             'groupname' => null,
             'childs' => false,
             'divider' => true,
         ];
 
+
+        $seoMenu = [
+            'slug' => 'Static pages',
+            'icon' => 'fa fa-list-alt',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Static pages'),
+            'groupname' => trans('dashboard::menu.Search Engine Optimization'),
+            'childs' => false,
+            'divider' => false,
+        ];
+
+
+
+        $siteMapMenu = [
+            'slug' => 'Site Map',
+            'icon' => 'fa fa-sitemap',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Site Map'),
+            'childs' => false,
+            'divider' => true,
+        ];
+
+
+
+        $typePostMenu = [
+            'slug' => 'Types posts',
+            'icon' => 'fa fa-pencil-square-o',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Types posts'),
+            'childs' => false,
+            'divider' => false,
+        ];
+
+        $categoryMenu = [
+            'slug' => 'Categories',
+            'icon' => 'fa fa-briefcase',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Categories'),
+            'childs' => false,
+            'divider' => false,
+        ];
+
+
+        $tagsMenu = [
+            'slug' => 'Tags',
+            'icon' => 'fa fa-tags',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Tags'),
+            'childs' => false,
+            'divider' => true,
+        ];
+
+        $menuMenu = [
+            'slug' => 'Menu',
+            'icon' => 'fa fa-bars',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Menu'),
+            'groupname' => trans('dashboard::menu.Posts Managements'),
+            'childs' => false,
+            'divider' => false,
+        ];
+
+
+
+
+        $usersMenu = [
+            'slug' => 'Users',
+            'icon' => 'fa fa-users',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Users'),
+            'groupname' => trans('dashboard::menu.Users'),
+            'childs' => false,
+            'divider' => false,
+        ];
+
+        $groupsMenu = [
+            'slug' => 'Groups',
+            'icon' => 'fa fa-lock',
+            'url' => route('dashboard.domains.index'),
+            'label' => trans('dashboard::menu.Groups'),
+            'childs' => false,
+            'divider' => true,
+        ];
+
+
+
+
+
+        $dashboardMenu->add('Tools', 'dashboard::partials.leftMenu', $menuMenu, 1);
+        $dashboardMenu->add('Tools', 'dashboard::partials.leftMenu', $typePostMenu, 2);
+        $dashboardMenu->add('Tools', 'dashboard::partials.leftMenu', $categoryMenu, 3);
+        $dashboardMenu->add('Tools', 'dashboard::partials.leftMenu', $tagsMenu, 4);
+
+        $dashboardMenu->add('Tools', 'dashboard::partials.leftMenu', $usersMenu, 5);
+        $dashboardMenu->add('Tools', 'dashboard::partials.leftMenu', $groupsMenu, 6);
+
+
+        $dashboardMenu->add('Tools', 'dashboard::partials.leftMenu', $seoMenu, 10);
+        $dashboardMenu->add('Tools', 'dashboard::partials.leftMenu', $siteMapMenu, 30);
+
+
+        $dashboardMenu->add('Systems', 'dashboard::partials.leftMenu', $errorMenu, 500);
         $dashboardMenu->add('Systems', 'dashboard::partials.leftMenu', $localizationMenu, 300);
         $dashboardMenu->add('Systems', 'dashboard::partials.leftMenu', $domainsMenu, 1);
     }
