5e18abd3a34378f628085a3be8eff42621b926b5
tabuna <bliz48rus@gmail.com>
2016-03-14 02:07:49 +0300

    fix

--- Resources/views/layouts/dashboard.blade.php
+++ Resources/views/layouts/dashboard.blade.php
@@ -118,7 +118,7 @@
     <!-- footer -->
     <footer id="footer" class="app-footer" role="footer">
         <div class="wrapper b-t bg-light">
-            <span class="pull-right">2.2.0
+            <span class="pull-right">{{orchid_version()}}
                 <a href="" class="m-l-sm text-muted"> <i class="fa fa-github"></i></a>
                 <a href="" class="m-l-sm text-muted"> <i class="fa fa-long-arrow-up"></i></a>
             </span>
--- helpers.php
+++ helpers.php
@@ -1,2 +1,11 @@
 <?php
 
+if (! function_exists('orchid_version')) {
+    /**
+     * @return string
+     */
+    function orchid_version()
+    {
+        return "0.0.1";
+    }
+}
