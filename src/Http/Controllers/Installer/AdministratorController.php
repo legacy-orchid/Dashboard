96e3432c4d80ee328137c4c73a87bc841fca2982
tabuna <bliz48rus@gmail.com>
2016-06-11 18:07:54 +0300

    Install fix

--- src/Http/Controllers/Installer/AdministratorController.php
+++ src/Http/Controllers/Installer/AdministratorController.php
@@ -4,7 +4,7 @@ namespace Orchid\Dashboard\Http\Controllers\Installer;
 
 use Illuminate\Routing\Redirector;
 use Orchid\Dashboard\Http\Controllers\Controller;
-use Orchid\Dashboard\Http\Requests\Request;
+use Illuminate\Http\Request;
 use Artisan;
 
 class AdministratorController extends Controller
@@ -16,17 +16,20 @@ class AdministratorController extends Controller
     {
         return view('dashboard::container.install.administrator');
     }
-    
+
     /**
      * @param Request $input
      * @param Redirector $redirect
      */
-    public function create(Request $input, Redirector $redirect)
+    public function create(Request $request, Redirector $redirect)
     {
-        $exitCode = Artisan::call('make:admin');
-        dd($exitCode);
+        $exitCode = Artisan::call('make:admin', [
+            'name' =>  $request->input('name'),
+            'email' =>  $request->input('email'),
+            'password' =>  $request->input('password'),
+        ]);
 
-        return redirect()->route('dashboard::finish')
+        return redirect()->route('dashboard::final')
             ->with(['message' => $exitCode]);
 
     }
